ad8c71c5ad35bfdc6060b322e763773668d9df3a
Resonanceclagame <volkaevj34963@gmail.com>
2026-08-27 20:09:10 +1000

    Add expense-tracker task to finalpool

---
+++ tasks/finalpool/expense-tracker/preprocess/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Preprocess script for expense-tracker task."""
+
+def preprocess():
+    """Prepare files for the task."""
+    print("Preprocessing expense-tracker task...")
+    return True
