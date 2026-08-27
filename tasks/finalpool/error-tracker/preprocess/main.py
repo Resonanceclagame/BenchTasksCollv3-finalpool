fa7242072ab1982255d4fafec6d861f1432fe135
Resonanceclagame <volkaevj34963@gmail.com>
2026-08-27 20:09:19 +1000

    Add error-tracker task to finalpool

---
+++ tasks/finalpool/error-tracker/preprocess/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Preprocess script for error-tracker task."""
+
+def preprocess():
+    """Prepare files for the task."""
+    print("Preprocessing error-tracker task...")
+    return True
