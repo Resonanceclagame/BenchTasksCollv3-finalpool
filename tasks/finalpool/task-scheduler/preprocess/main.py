8ca2f711fb5907f60e687e635422a8a53e9bd30f
Resonanceclagame <volkaevj34963@gmail.com>
2026-08-27 20:09:30 +1000

    Add task-scheduler task to finalpool

---
+++ tasks/finalpool/task-scheduler/preprocess/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Preprocess script for task-scheduler task."""
+
+def preprocess():
+    """Prepare files for the task."""
+    print("Preprocessing task-scheduler task...")
+    return True
